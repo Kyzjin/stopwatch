dd163dcf26abb311795c5222ec97831206b65b71
Kyzjin <jparish@vandals.uidaho.edu>
2015-03-10 17:33:25 -0700

    a
    a

---
+++ build/android/gen/com/packtpub/hotshot/stopwatch/StopwatchAppInfo.java
@@ -0,0 +1,70 @@
+package com.packtpub.hotshot.stopwatch;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class StopwatchAppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public StopwatchAppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.packtpub.hotshot.stopwatch";
+	}
+
+	public String getName() {
+		return "StopWatch";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "Kyzjin";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by Kyzjin";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "44816dc5-37e9-4801-b6d9-40b068058a6c";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
